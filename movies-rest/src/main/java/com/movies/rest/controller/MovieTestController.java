9ff42ef9b4d926639303e4ea0becf04b1fb2184f
amandaglio <a.mandaglio@gmail.com>
2016-09-25 14:09:56 +0200

    Correcciones

--- movies-rest/src/main/java/com/movies/rest/controller/MovieTestController.java
+++ movies-rest/src/main/java/com/movies/rest/controller/MovieTestController.java
@@ -1,41 +1,35 @@
 package com.movies.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.movies.rest.domain.FilmDTO;
 import com.movies.service.IMovieServiceTest;
 
+import ma.glasnost.orika.MapperFacade;
+
 @RestController
 @RequestMapping("/test")
-public class MovieTest {
+public class MovieTestController {
 
 	@Inject
 	private IMovieServiceTest movieServiceTest;
 	
-	@RequestMapping(value="/movieTest")
+	@Inject
+	private MapperFacade mapper;
+	
+	@RequestMapping(value="/create")
 	public void createMovie(){
 		movieServiceTest.createMovie();
 	}
 	
 	@RequestMapping(value="/getAll", method = RequestMethod.GET)
-	@ResponseBody
 	public List<FilmDTO> getAll(){
-		List<FilmDTO> pija = new ArrayList<>();
-		pija.add(new FilmDTO());
-		pija.add(new FilmDTO());
-		pija.add(new FilmDTO());
-		pija.add(new FilmDTO());
-		System.out.println();
-//		List<MovieDTO>  algomas = mapper.mapAsList(movieServiceTest.getAllMovies(), MovieDTO.class);
-		return pija;
-		
+		return mapper.mapAsList(movieServiceTest.getAllMovies(), FilmDTO.class);
 	}
 }
