ccb460ea3c40b8e32b80793ed2e6ca58e03f9761
tano14 <taneando@gmail.com>
2016-10-01 20:23:23 +0200

    Cambios en la cofiguracion

--- movies-repository/src/main/java/com/movies/repository/config/RepositoryConfig.java
+++ movies-repository/src/main/java/com/movies/repository/config/RepositoryConfig.java
@@ -3,6 +3,7 @@ package com.movies.repository.config;
 import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@ComponentScan(basePackages="com.movies.repository")
 @EnableJpaRepositories("com.movies.repository")
 public class RepositoryConfig {
 
--- movies-rest/src/main/java/com/movies/rest/config/WebAppInitializer.java
+++ movies-rest/src/main/java/com/movies/rest/config/WebAppInitializer.java
@@ -5,34 +5,23 @@ import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import com.movies.domain.config.DomainConfig;
-import com.movies.repository.config.RepositoryConfig;
-import com.movies.service.config.ServiceConfig;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext container) {
-
-		// Create the 'root' Spring application context
-		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-		rootContext.register(RestConfig.class, ServiceConfig.class, DomainConfig.class, RepositoryConfig.class);
-		// rootContext.register(ServiceConfig.class, JPAConfig.class,
-		// SecurityConfig.class);
-
-		// Manage the lifecycle of the root application context
-		container.addListener(new ContextLoaderListener(rootContext));
-
-		// Create the dispatcher servlet's Spring application context
-		AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
-		dispatcherServlet.register(RestConfig.class);
-
-		// Register and map the dispatcher servlet
-		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher",
-				new DispatcherServlet(dispatcherServlet));
-		dispatcher.setLoadOnStartup(1);
-		dispatcher.addMapping("/");
+        WebApplicationContext context = getContext();
+        container.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = container.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/*");
 	}
+	
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.movies");
+        return context;
+    }
 }
